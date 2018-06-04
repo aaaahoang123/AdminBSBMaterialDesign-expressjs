db0b804c083099a3f4aa8da0c58038068f20a370
Hoang <shinkenger.vn9x@gmail.com>
2018-06-04 22:19:13 +0700

    fix routing

--- routes/widgets.js
+++ routes/widgets.js
@@ -1,7 +1,34 @@
 const express = require('express');
 const router = express.Router({});
 
-router.use('/cards', require('./widgets-sub/cards'))
-    .use('/info-box', require('./widgets-sub/info-box'));
+/**
+ * Routing the widgets/cards/* resource
+ */
+router.use('/cards', express.Router({})
+    .get('/basic', (req, res) => {
+        res.render('pages/widgets/cards/basic', {path: '/widgets/cards/basic'});
+    }).get('/colored', (req, res) => {
+        res.render('pages/widgets/cards/colored', {path: '/widgets/cards/colored'});
+    }).get('/no-header', (req, res) => {
+        res.render('pages/widgets/cards/no-header', {path: '/widgets/cards/no-header'});
+    })
+);
+
+/**
+ * Routing the widgets/info-box/* resource
+ */
+router.use('/info-box', express.Router({})
+    .get('/info-box-1', (req, res) => {
+        res.render('pages/widgets/info-box/info-box-1', {path: '/widgets/info-box/info-box-1'});
+    }).get('/info-box-2', (req, res) => {
+        res.render('pages/widgets/info-box/info-box-2', {path: '/widgets/info-box/info-box-2'});
+    }).get('/info-box-3', (req, res) => {
+        res.render('pages/widgets/info-box/info-box-3', {path: '/widgets/info-box/info-box-3'});
+    }).get('/info-box-4', (req, res) => {
+        res.render('pages/widgets/info-box/info-box-4', {path: '/widgets/info-box/info-box-4'});
+    }).get('/info-box-5', (req, res) => {
+        res.render('pages/widgets/info-box/info-box-5', {path: '/widgets/info-box/info-box-5'});
+    })
+);
 
 module.exports = router;
